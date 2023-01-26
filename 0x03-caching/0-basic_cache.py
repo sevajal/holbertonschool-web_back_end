18e82eaeefbd37b7277ca9ba17ab1c849d42f384
sevajal <sevajal@yahoo.com>
2023-01-26 09:38:45 -0500

    I did the task 0

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 0-main """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Class for BasicCache"""
+    def put(self, key, item):
+        """Insert data into cache_data"""
+        if key and item:
+            self.cache_data[key] = item
+    def get(self, key):
+        """Obtain data from cache_data"""
+        if key and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
