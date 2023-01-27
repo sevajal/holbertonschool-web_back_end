f83d9cfb909ede962374ba65097ca965385b5a32
sevajal <sevajal@yahoo.com>
2023-01-26 23:17:16 -0500

    Recovering deleted files

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""1. Let's execute multiple coroutines at the same time with async"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """async routine called wait_n that takes in 2 int arguments
+    (in this order): n and max_delay. You will spawn wait_random
+    n times with the specified max_delay. wait_n should return
+    the list of all the delays (float values). The list of the
+    delays should be in ascending order without using sort()
+    because of concurrency."""
+    delays = [wait_random(max_delay) for i in range(0, n)]
+    sorted_delays = []
+    for await_random in asyncio.as_completed(delays):
+        delay = await await_random
+        sorted_delays.append(delay)
+    else:
+        return sorted_delays
