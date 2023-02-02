52c1d42b32c70b1b6eb1d5ef979f2d3958b150dd
sevajal <sevajal@yahoo.com>
2023-02-02 09:45:27 -0500

    I did the task 1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Filtered logger"""
 from typing import List
+import logging
 import re
 
 
@@ -11,3 +12,25 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub("(?<={:s}=)(.*?)(?={:s})".format(string, separator),
                          redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Init Redacting Formatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Formats the class object"""
+        message = record.getMessage()
+        message = filter_datum(self.fields, self.REDACTION, message,
+                               self.SEPARATOR)
+        record = logging.LogRecord("my_logger", logging.INFO, None, None,
+                                   message, None, None)
+        return super(RedactingFormatter, self).format(record)
