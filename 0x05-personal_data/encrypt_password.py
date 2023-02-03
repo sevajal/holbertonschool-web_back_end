686b058383ed37937f1a2b6fee3e1406bff0e742
sevajal <sevajal@yahoo.com>
2023-02-03 10:06:32 -0500

    I did the task 6

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -6,4 +6,9 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """ returns a salted, hashed password, which is a byte string """
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that the provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
