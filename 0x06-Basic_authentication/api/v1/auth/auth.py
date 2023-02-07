e456794cece111a8c243eb72c6dbb887c9dd5e92
sevajal <sevajal@yahoo.com>
2023-02-07 09:08:31 -0500

    First draft task 3

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Module for basic authenticator """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Class for basic authenticator """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Public method that requires auth"""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ Public method for authorization header """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Public method for current user """
+        return None
