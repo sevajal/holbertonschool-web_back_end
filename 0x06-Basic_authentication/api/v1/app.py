f13d320c53fe342c11408ff26a58bed188d0b9da
sevajal <sevajal@yahoo.com>
2023-02-07 23:27:43 -0500

    I did the task 5

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -12,6 +12,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if os.getenv("AUTH_TYPE"):
+    auth = os.getenv("AUTH_TYPE")
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -33,6 +38,21 @@ def forbidden(error) -> str:
     """ Forbidden error
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    print("auth:", auth)
+    if auth is None:
+        return
+    if auth.require_auth(request.path,
+                         ['/api/v1/status/', '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']) is False:
+        return
+    if auth.authorization_header(request) is None:
+        return unauthorized(401)
+    if auth.current_user(request) is None:
+        return forbidden(403)
 
 
 if __name__ == "__main__":
--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -19,7 +19,9 @@ class Auth():
 
     def authorization_header(self, request=None) -> str:
         """ Public method for authorization header """
-        return None
+        if request is None or request.headers.get("Authorization") is None:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Public method for current user """
