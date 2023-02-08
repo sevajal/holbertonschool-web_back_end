dae8a0f9e8b5b81deb68f2012c6a3af71f6f574f
sevajal <sevajal@yahoo.com>
2023-02-07 23:41:50 -0500

    I did the task 6

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -15,8 +15,12 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if os.getenv("AUTH_TYPE"):
     auth = os.getenv("AUTH_TYPE")
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+    if auth == "auth":
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+    if auth == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module for basic authenticator """
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic Auth """
+    pass
