be7115e91fba254b023f3a7f0bfe7a0ab576f0e1
sevajal <sevajal@yahoo.com>
2023-02-09 22:45:48 -0500

    I did the task 6

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -30,3 +30,9 @@ class SessionAuth(Auth):
             return None
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
+
+    def current_user(self, request=None):
+        """ returns a User instance based on a cookie value """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
