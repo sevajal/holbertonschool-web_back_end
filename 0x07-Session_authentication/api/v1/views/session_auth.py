3d111dd836819d3f284d94103b73015332ed149c
sevajal <sevajal@yahoo.com>
2023-02-09 23:04:34 -0500

    First draft task 7

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Module of Session_auth views
+"""
+from api.v1.views import app_views
+from flask import jsonify, request
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """login a user
+    """
+    data = request.form
+    if not data.get('email'):
+        return jsonify({"error": "email missing"}), 400
+    if not data.get('password'):
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({'email': data.get('email')})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user[0].is_valid_password(data.get('password')):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user[0].id)
+    response = jsonify(user[0].to_json())
+    response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+    return response
