54c6f56a5e7aff125670e60cc6a0c6a0ad39c2f8
sevajal <sevajal@yahoo.com>
2023-02-09 22:37:49 -0500

    First draft task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -54,8 +54,12 @@ def before_request():
         return
     if auth.require_auth(request.path,
                          ['/api/v1/status/', '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/']) is False:
+                          '/api/v1/forbidden/',
+                          '/api/v1/auth_session/login/']) is False:
         return
+    if auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
+        abort(401)
     if auth.authorization_header(request) is None:
         return unauthorized(401)
     if auth.current_user(request) is None:
