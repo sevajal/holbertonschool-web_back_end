c7c7ada0c5dd3498ca122bae14867f4f4ce5b24a
sevajal <sevajal@yahoo.com>
2023-02-13 21:30:46 -0500

    Draft task 6

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" App flask """
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
