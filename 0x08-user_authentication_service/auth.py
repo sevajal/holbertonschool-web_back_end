8e5566e4983a36574bc3a8644ef1d41d38d16c67
sevajal <sevajal@yahoo.com>
2023-02-13 21:22:45 -0500

    Draft task 5

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 """ 4. Hash password """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """ Return a hash password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """ Auth class to interact with the authentication database. """
+    def __init__(self):
+        """ Init method """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a user """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hash_password = _hash_password(password)
+            user = self._db.add_user(email, hash_password)
+            return user
+        else:
+            raise ValueError(f'User {email} already exists')
