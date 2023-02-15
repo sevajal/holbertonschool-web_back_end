fe3cfdb0f56fd2ed3ce64ccd1d65fd996a62679f
sevajal <sevajal@yahoo.com>
2023-02-14 20:04:35 -0500

    Draft task 19

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -73,5 +73,18 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """ Updates the password"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_toke')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except Exception:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
