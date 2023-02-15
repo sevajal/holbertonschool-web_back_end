07fa4eddab88a08390e5210b8b74b56d474333aa
sevajal <sevajal@yahoo.com>
2023-02-14 19:59:28 -0500

    Draft task 18

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -78,3 +78,13 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Updates the password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        new_pwd = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=new_pwd,
+                             reset_token=None)
