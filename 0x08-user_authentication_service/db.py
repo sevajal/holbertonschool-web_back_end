ea6b9a23e41a983f1b1d6cc13c33088a87ab0903
sevajal <sevajal@yahoo.com>
2023-02-15 17:44:38 -0500

    Testing

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -29,11 +29,11 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ add a user to the session """
-        new_user = User(email=email, hashed_password=hashed_password)
-        DB._session
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
+        if email and hashed_password:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+            return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """ Finds an user by arguments"""
