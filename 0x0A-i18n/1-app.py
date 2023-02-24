cf7a0785f753b098adb6765e95b9c27a38ff9dfb
sevajal <sevajal@yahoo.com>
2023-02-23 21:26:58 -0500

    Draft task 1

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" 1. Basic Babel setup """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Config class for Babel object """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def hello_world():
+    """ Render a html file """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
