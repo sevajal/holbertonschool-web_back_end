5ba007c6f0b74b0d82f59f8e945b7a88ba0df287
sevajal <sevajal@yahoo.com>
2023-03-06 22:16:36 -0500

    Draft task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """0x0B redis basic"""
 
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 import uuid
 
 
@@ -19,3 +19,18 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float]:
+        """Gets a key from Redis and change the format"""
+        if fn:
+            return fn(self._redis.get(key))
+        return (self._redis.get(key))
+
+    def get_str(self, key: str) -> str:
+        """Returns a str from redis"""
+        return (self.get(key, str))
+
+    def get_int(self, key: str) -> int:
+        """Returns an int from redis"""
+        return (self.get(key, int))
