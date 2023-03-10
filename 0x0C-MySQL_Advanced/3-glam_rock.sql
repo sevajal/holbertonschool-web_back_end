ee52b93fa5eb6f3e7b795144e05710d20f2541ff
sevajal <sevajal@yahoo.com>
2023-03-09 22:01:00 -0500

    I did the task 3

---
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+SELECT band_name, IF(split, split - formed, year(curdate()) - formed) AS lifespan 
+FROM metal_bands 
+WHERE style LIKE '%Glam rock%' 
+ORDER BY lifespan DESC;
