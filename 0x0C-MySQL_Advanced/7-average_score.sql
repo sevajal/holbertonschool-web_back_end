61037628e10ece2969ef93ee1a599306d52061fc
sevajal <sevajal@yahoo.com>
2023-03-09 22:20:28 -0500

    Second draft task 7

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,7 +1,7 @@
 -- Creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(user_id int)
-BEGIN SET @average = (SELECT AVG(score) FROM corrections GROUP BY user_id HAVING user_id = user_id);
-UPDATE users SET average_score = @average WHERE id = user_id;
+CREATE PROCEDURE ComputeAverageScoreForUser(arg_user_id int)
+BEGIN SET @average = (SELECT AVG(score) FROM corrections GROUP BY user_id HAVING user_id = arg_user_id);
+UPDATE users SET average_score = @average WHERE id = arg_user_id;
 END $$
 DELIMITER;
