c246d39da774066d749905bda8fe073f71ba13e5
sevajal <sevajal@yahoo.com>
2023-03-14 19:51:35 -0500

    Draft 4 task 12

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -3,13 +3,12 @@
 from pymongo import MongoClient
 
 if __name__ == '__main__':
-    mongo_client = MongoClient("mongodb://localhost:27017/")
-    mongo_collection = mongo_client.logs.nginx
-    print(f"{mongo_collection.estimated_document_count()} logs")
+    client = MongoClient("mongodb://localhost:27017")
+    collection = client.logs.nginx
+    print(f"{collection.estimated_document_count()} logs")
     print('Methods:')
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        logs_count = mongo_collection.count_documents({'method': method})
+        logs_count = collection.count_documents({'method': method})
         print(f'\tmethod {method}: {logs_count}')
-    docs = mongo_collection.count_documents({'method': 'GET',
-                                            'path': '/status'})
+    docs = collection.count_documents({'method': 'GET', 'path': '/status'})
     print(f'{docs} status check')
