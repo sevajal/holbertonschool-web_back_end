fd48c28109af851d3d651b7aad2d0832a9030988
sevajal <sevajal@yahoo.com>
2023-03-20 21:31:41 -0500

    Draft task 6

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,33 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, filename) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(filename);
+  const promise = [];
+  await user.then((value) => {
+    promise.push({
+      status: 'fulfilled',
+      value,
+    });
+  })
+  .catch((error) => {
+    promise.push({
+      status: 'rejected',
+      value: `${error.name}`
+    });
+  });
+  await photo.then((value) => {
+    promise.push({
+      status: 'fullfilled',
+      value,
+    });
+  })
+  .catch((error) => {
+    promise.push({
+      status: 'rejected',
+      value: `${error.name}`,
+    });
+  });
+  return promise;
+}
