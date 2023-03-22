237478a4e584a826d3714f550f952553e1814e5a
sevajal <sevajal@yahoo.com>
2023-03-21 20:04:54 -0500

    Style

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup() {
   return Promise.all([photo, user]).then((resolve) => {
     console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);
   })
-  .catch(() => {
-    console.log('Signup system offline');
-  });
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
