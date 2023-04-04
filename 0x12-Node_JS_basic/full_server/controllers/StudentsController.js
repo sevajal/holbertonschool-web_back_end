e21e24da93b0706e198bc57a547225893c12d437
sevajal <sevajal@yahoo.com>
2023-04-03 21:17:45 -0500

    Draft task 8

---
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,41 @@
+import readDatabase from '../utils';
+
+export default class StudentsController {
+  static getAllStudents(request, response, db) {
+    readDatabase(db)
+      .then((studentsByField) => {
+        response.write('This is the list of our students');
+        let resStudent = '';
+        for (const field in studentsByField) {
+          let studentsList = '';
+          for (const student of studentsByField[field]) {
+            if (studentsList.length > 0) {
+              studentsList += ', ';
+            }
+            studentsList += student;
+          }
+          resStudent += `Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsList}\n`;
+        }
+        response.write(resStudent.slice(0, -1));
+        response.send(200);
+      })
+      .catch(() => {
+        response.send(500, 'Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(request, response, db) {
+    const { major } = request.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      response.send(500, 'Major parameter must be CS or SWE');
+    } else {
+      readDatabase(db)
+        .then((fields) => {
+          const students = fields[major];
+          response.send(200, `List: ${students.join(', ')}`);
+        })
+        .catch(() => response.send(500, 'Cannot load the database'));
+    }
+  }
+}
---
+++ 0x12-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+async function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    try {
+      const data = fs.readFile(path, 'utf8');
+      const lines = data.split('\n');
+      const studentsByField = {};
+      lines.shift();
+      for (const line of lines) {
+        const lineArray = line.split(',');
+        if (lineArray.length === 4) {
+          if (lineArray[3] in studentsByField) {
+            studentsByField[lineArray[3]].push(lineArray[0]);
+          } else {
+            studentsByField[lineArray[3]] = [lineArray[0]];
+          }
+        }
+      }
+      resolve(studentsByField);
+    } catch (err) {
+      reject(new Error('Cannot load the database'));
+    }
+  });
+}
+
+module.exports = readDatabase;
