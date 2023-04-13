9f999f2abc9153f6ded48874ee2dc1a60a5c1fc3
sevajal <sevajal@yahoo.com>
2023-04-12 20:46:44 -0500

    Draft task 2

---
+++ 0x14-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,24 @@
+import * as redis from 'redis';
+
+const client = redis.createClient();
+
+client.on("error", (error) => {
+  console.log(`Redis client not connected to the server: ${error}`)
+});
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, res) => {
+    console.log(res);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
